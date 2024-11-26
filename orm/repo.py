76848bb41107bae178b615b2ce3636732f94efad
dulcemcm <dulce.cortes@cua.uam.mx>
2024-11-26 17:55:15 -0600

    Update repo.py - Ejercicio GET terminado

--- orm/repo.py
+++ orm/repo.py
@@ -10,8 +10,8 @@ def usuario_por_id(sesion:Session,id_usuario:int):
 
 def compra_por_id(sesion:Session,id_compra:int):
     print("select * from app.compras where id = ", id_compra)
-    return sesion.query(modelos.Usuario).filter(modelos.Usuario.id==id_compra).first()
+    return sesion.query(modelos.Compra).filter(modelos.Compra.id==id_compra).first()
 
 def foto_por_id(sesion:Session,id_foto:int):
     print("select * from app.fotos where id = ", id_foto)
-    return sesion.query(modelos.Usuario).filter(modelos.Usuario.id==id_foto).first()
+    return sesion.query(modelos.Foto).filter(modelos.Foto.id==id_foto).first()
